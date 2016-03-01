2b00bb5c3984a1a865e685da88cf774c90c2dc10
JeroenSormani <jeroen.sormani@gmail.com>
2016-03-01 17:50:45 +0100

    Code consistency changes for includes/class-note-post-type.php

--- includes/class-note-post-type.php
+++ includes/class-note-post-type.php
@@ -24,7 +24,7 @@ class Note_Post_Type {
 		// Register post type
 		add_action( 'init', array( $this, 'register_post_type' ) );
 
-	 }
+		}
 
 
 	/**
@@ -37,32 +37,33 @@ class Note_Post_Type {
 	public function register_post_type() {
 
 		$labels = array(
-			'name'					=> __( 'Notes', 'wp-dashboard-notes' ),
-			'singular_name'			=> __( 'Note', 'wp-dashboard-notes' ),
-			'add_new'				=> __( 'Add New', 'wp-dashboard-notes' ),
-			'add_new_item'			=> __( 'Add New Note', 'wp-dashboard-notes' ),
-			'edit_item'				=> __( 'Edit Note', 'wp-dashboard-notes' ),
-			'new_item'				=> __( 'New Note', 'wp-dashboard-notes' ),
-			'view_item'				=> __( 'View Note', 'wp-dashboard-notes' ),
-			'search_items'			=> __( 'Search Notes', 'wp-dashboard-notes' ),
-			'not_found'				=> __( 'No Notes', 'wp-dashboard-notes' ),
-			'not_found_in_trash'	=> __( 'No Notes found in Trash', 'wp-dashboard-notes' ),
+			'name'               => __( 'Notes', 'wp-dashboard-notes' ),
+			'singular_name'      => __( 'Note', 'wp-dashboard-notes' ),
+			'add_new'            => __( 'Add New', 'wp-dashboard-notes' ),
+			'add_new_item'       => __( 'Add New Note', 'wp-dashboard-notes' ),
+			'edit_item'          => __( 'Edit Note', 'wp-dashboard-notes' ),
+			'new_item'           => __( 'New Note', 'wp-dashboard-notes' ),
+			'view_item'          => __( 'View Note', 'wp-dashboard-notes' ),
+			'search_items'       => __( 'Search Notes', 'wp-dashboard-notes' ),
+			'not_found'          => __( 'No Notes', 'wp-dashboard-notes' ),
+			'not_found_in_trash' => __( 'No Notes found in Trash', 'wp-dashboard-notes' ),
 		);
 
 		register_post_type( 'note', array(
-			'label'					=> 'note',
-			'show_ui'				=> false,
-			'show_in_menu'			=> false,
-			'capability_type'		=> 'post',
-			'map_meta_cap'			=> true,
-			'rewrite'				=> array( 'slug' => 'notes' ),
-			'_builtin'				=> false,
-			'query_var'				=> true,
-			'supports'				=> array( 'title', 'editor' ),
-			'labels'				=> $labels,
+			'label'           => 'note',
+			'show_ui'         => false,
+			'show_in_menu'    => false,
+			'capability_type' => 'post',
+			'map_meta_cap'    => true,
+			'rewrite'         => array( 'slug'         => 'notes' ),
+			'_builtin'        => false,
+			'query_var'       => true,
+			'supports'        => array( 'title', 'editor' ),
+			'labels'          => $labels,
 		) );
 
 	}
+
 
 }
 
